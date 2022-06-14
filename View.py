bbec2b9c9a0ab4378ec89f9b2596e3d4ead936c3
{James:Richards} <78932198+JamesKr13@users.noreply.github.com>
2022-06-14 12:17:11 +1200

    Add files via upload
    Minor Bug with removing and adding items where item adds the wrong price

---
+++ View.py
@@ -0,0 +1,61 @@
+
+from Controller import *
+from PySide6.QtWidgets import *
+from PySide6.QtCore import Qt
+app = QApplication()
+# window = QMainWindow()
+class mainwindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self, None, Qt.WindowStaysOnTopHint)
+layout = QGridLayout()
+window = mainwindow()
+window.setWindowTitle("Shop")
+window.setCentralWidget(QWidget())
+window.centralWidget().setLayout(layout)
+
+
+
+#Allows for user to alter current day
+allow_alter = False
+debug = QSpinBox()
+if input("Debug mode Y/N").lower() == "y":
+    layout.addWidget(debug)
+    allow_alter = True
+
+#add widgets
+catergories = QComboBox()
+ComboBox = QComboBox()
+item_name = QLabel()
+# item_days_avaliable = QLabel()
+item_price = QLabel()
+item_vegan = QLabel()
+item_vege = QLabel()
+item_sugar = QLabel()
+add_order = QPushButton("Add")
+orders = QListWidget()
+total = QLabel()
+submit_order = QPushButton("Submit")
+
+
+
+layout.addWidget(catergories)
+layout.addWidget(ComboBox)
+layout.addWidget(item_name)
+# layout.addWidget(item_days_avaliable)
+layout.addWidget(item_price)
+layout.addWidget(item_vegan)
+layout.addWidget(item_vege)
+layout.addWidget(item_sugar)
+layout.addWidget(add_order)
+layout.addWidget(orders)
+layout.addWidget(total)
+layout.addWidget(submit_order)
+
+
+vc = controller(allow_alter,debug,catergories,ComboBox,item_name,item_price,item_vegan,item_vege,item_sugar,add_order,orders,total,submit_order)
+vc.view_load()
+
+window.show()
+
+
+app.exec()
